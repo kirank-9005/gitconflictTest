88b64a32974623f6601f15c7f28421ba4d430b26
kalokhekk <kalokhekk@gmail.com>
2023-07-28 15:34:49 +0530

    class1 file added

---
+++ src/Class1.java
@@ -0,0 +1,12 @@
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("line1");
+		System.out.println("line2");
+		System.out.println("line3");
+		System.out.println("line4");
+
+	}
+
+}
